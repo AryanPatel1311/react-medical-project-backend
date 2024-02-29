afee696f4fd6229f83b845182ae2b15db918e31a
Aryan <aryanbpatel19@gnu.ac.in>
2024-02-29 15:22:05 +0530

    "api changes"

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import userRoute from "./Routes/user.js";
 import doctorRoute from "./Routes/doctors.js";
 import reviewRoute from "./Routes/review.js";
 import bookingRoute from "./Routes/booking.js";
+
 dotenv.config();
 
 const app = express();
@@ -17,25 +18,7 @@ const corsOptions = {
   origin: true,
 };
 
-app.get("/", (req, res) => {
-  res.send("Api is working");
-});
-
-// database connection new
-mongoose.set("strictQuery", false);
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB database is connected");
-  } catch (err) {
-    console.log("MongoDB database is connection faild");
-  }
-};
-
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
@@ -45,7 +28,31 @@ app.use("/api/v1/doctors", doctorRoute);
 app.use("/api/v1/reviews", reviewRoute);
 app.use("/api/v1/bookings", bookingRoute);
 
+// Database connection
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB database is connected");
+  } catch (err) {
+    console.error("MongoDB database connection failed", err);
+    // Throw the error if you want to stop the server on connection failure
+    // throw err;
+  }
+};
+
+// Move the database connection call outside the app.listen callback
+connectDB();
+
+// API root endpoint
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+// Start the server
 app.listen(port, () => {
-  connectDB();
-  console.log("Server is running on port" + port);
+  console.log("Server is running on port " + port);
 });
