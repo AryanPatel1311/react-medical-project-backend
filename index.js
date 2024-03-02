6648121046aefef3421a82af152c75d3e47562eb
Aryan <aryanbpatel19@gnu.ac.in>
2024-03-02 14:32:13 +0530

    "solved 3rd problem"

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-// Import necessary modules
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import authRoute from "./Routes/auth.js";
 import userRoute from "./Routes/user.js";
 import doctorRoute from "./Routes/doctors.js";
 import reviewRoute from "./Routes/review.js";
 import bookingRoute from "./Routes/booking.js";
-import cors from "cors";
-
-// Load environment variables
 dotenv.config();
 
-// Create Express app
 const app = express();
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 8000;
 
-// Middleware
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: true,
+};
 
-// Database connection
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+// database connection new
 mongoose.set("strictQuery", false);
-
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
@@ -39,25 +31,21 @@ const connectDB = async () => {
     });
     console.log("MongoDB database is connected");
   } catch (err) {
-    console.error("MongoDB database connection failed", err);
+    console.log("MongoDB database is connection faild");
   }
 };
 
-connectDB();
-
-// Routes
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/doctors", doctorRoute);
 app.use("/api/v1/reviews", reviewRoute);
 app.use("/api/v1/bookings", bookingRoute);
 
-// API root endpoint
-app.get("/", (req, res) => {
-  res.send("API is working");
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log("Server is running on port " + port);
+  connectDB();
+  console.log("Server is running on port" + port);
 });
